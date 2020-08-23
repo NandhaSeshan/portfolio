79afed2095bcac8cddc4ed51b6ea8ca621befcef
Nandha Seshan <gsesha@icims.com>
2020-08-23 20:34:09 +0530

    Added icons

--- src/about/About.js
+++ src/about/About.js
@@ -2,54 +2,69 @@ import React from "react";
 import Fade from "react-reveal";
 import Flash from "react-reveal";
 import Logo from "../images/pngegg.png";
+import Typography from "@material-ui/core/Typography";
+import CodeIcon from "@material-ui/icons/Code";
+import CameraAltIcon from "@material-ui/icons/CameraAlt";
+import BrushIcon from "@material-ui/icons/Brush";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import DirectionsRunIcon from "@material-ui/icons/DirectionsRun";
+import SportsSoccerIcon from "@material-ui/icons/SportsSoccer";
 
 import "./About.css";
 
 class About extends React.Component {
   render() {
     return (
-      <div id="about">
-        <div className="about_me">
-          <div id="page_heading">
-            <Flash>
-              <p>About ME?</p>
-            </Flash>
-          </div>
-          <div id="thanos_gauntlet">
-            <img src={Logo} alt="hand_img" className="image" />
-          </div>
-          <div id="page_content1">
-            <Fade left>
-              <p>A developer / web designer</p>
-            </Fade>
-          </div>
-          <div id="page_content2">
-            <Fade right>
-              <p>A budding photographer</p>
-            </Fade>
-          </div>
-          <div id="page_content3">
-            <Fade right>
-              <p>Art enthusiast</p>
-            </Fade>
-          </div>
-          <div id="page_content4">
-            <Fade bottom>
-              <p>Amatuer singer</p>
-            </Fade>
-          </div>
-          <div id="page_content5">
-            <Fade left>
-              <p>Fittness enthusiast</p>
-            </Fade>
-          </div>
-          <div id="page_content6">
-            <Fade bottom>
-              <p>Amatuer footballer</p>
-            </Fade>
+      <React.Fragment>
+        <div id="about">
+          <div className="about_me">
+            <div id="page_heading">
+              <Flash>
+                <Typography variant="p">About ME?</Typography>
+              </Flash>
+            </div>
+            <div id="thanos_gauntlet">
+              <img src={Logo} alt="hand_img" className="image" />
+            </div>
+            <div id="page_content1">
+              <Fade left>
+                <CodeIcon fontSize="large" />
+                <Typography variant="p">A developer / web designer</Typography>
+              </Fade>
+            </div>
+            <div id="page_content2">
+              <Fade right>
+                <CameraAltIcon fontSize="large" />
+                <Typography variant="p">A budding photographer</Typography>
+              </Fade>
+            </div>
+            <div id="page_content3">
+              <Fade right>
+                <BrushIcon fontSize="large" />
+                <Typography variant="p">Art enthusiast</Typography>
+              </Fade>
+            </div>
+            <div id="page_content4">
+              <Fade left>
+                <MusicNoteIcon fontSize="large" />
+                <Typography variant="p">Amatuer singer</Typography>
+              </Fade>
+            </div>
+            <div id="page_content5">
+              <Fade bottom>
+                <DirectionsRunIcon fontSize="large" />
+                <Typography variant="p">Fittness enthusiast</Typography>
+              </Fade>
+            </div>
+            <div id="page_content6">
+              <Fade bottom>
+                <SportsSoccerIcon fontSize="large" />
+                <Typography variant="p">Amatuer footballer</Typography>
+              </Fade>
+            </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
---
+++ src/about/Skills.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Skills.css";
+import Typography from "@material-ui/core/Typography";
+
+export const Skills = () => (
+  <div className="skills">
+    <Typography variant="h4">Technical Skills</Typography>
+  </div>
+);
+
+export default Skills;
